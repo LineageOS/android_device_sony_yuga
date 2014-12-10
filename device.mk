5e59b9139ad6dbbebb3c916cbdc1e05dbb8b2457
Ying Wang <wangying@google.com>
2014-12-10 12:44:33 -0800

    yuga: aapt: Set PRODUCT_AAPT_PREBUILT_DPI
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6

--- device.mk
+++ device.mk
@@ -17,6 +17,7 @@
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi 280dpi hdpi tvdpi mdpi ldpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Device specific overlays
