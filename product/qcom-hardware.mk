450d0860299c034a54e657db5b69801bdcf7d041
Jaekyun Seok <jaekyun@google.com>
2017-02-16 13:28:13 +0900

    vendor: Move device specific system configs into /vendor/etc
    Test: building succeeded and tested
    Bug: 35369237
    Change-Id: I9aaa226af76246d0e589049a9a8c9093f0267108

--- product/qcom-hardware.mk
+++ product/qcom-hardware.mk
@@ -1,3 +1,3 @@
 # Hardware permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
--- product/qcom-sensors.mk
+++ product/qcom-sensors.mk
@@ -1,3 +1,3 @@
 # Sensors permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml
