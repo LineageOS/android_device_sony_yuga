2f7d6c3849e920c7fc9d993ebaca3ac7c1a47686
Chippa-a <vusal1372@gmail.com>
2017-04-01 20:44:04 +0300

    yuga: Rebase the device tree into fragments structure
     * Based on new device tree structure introduced by
       Steve Kondik for Nextbit Robin (ether)
    
     * Also thanks to Adrian DC for his useful improvements
       introduced for huashan and blue-common devices
    
    Change-Id: I54f62d028b9c4cf3a1b6df1225886c70cc7cba1f
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,36 @@
-$(call inherit-product, device/sony/yuga/full_yuga.mk)
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit CM common Phone stuff.
+# Inherit common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.1.1/10.7.A.0.228/58103698:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.228 58103698 release-keys"
+# Inherit device configurations
+$(call inherit-product, device/sony/yuga/device.mk)
 
+# Device display
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Device identifications
 PRODUCT_NAME := lineage_yuga
 PRODUCT_DEVICE := yuga
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia Z
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.1.1/10.7.A.0.228/58103698:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.228 58103698 release-keys"
