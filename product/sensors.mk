d8b4dda697249baecf98d9eebb1e5736e637aab1
Chippa-a <vusal1372@gmail.com>
2017-04-01 20:44:04 +0300

    yuga: Rebase the device tree into fragments structure
     * Based on new device tree structure introduced by
       Steve Kondik for Nextbit Robin (ether)
    
     * Also thanks to Adrian DC for his useful improvements
       introduced for huashan and blue-common devices
    
    Change-Id: I54f62d028b9c4cf3a1b6df1225886c70cc7cba1f
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

---
+++ product/sensors.mk
@@ -0,0 +1,3 @@
+# Sensors configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors.conf:system/etc/sensors.conf
---
+++ product/thermal.mk
@@ -0,0 +1,3 @@
+# Thermal configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermanager.xml:system/etc/thermanager.xml
