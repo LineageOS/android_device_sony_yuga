7729719491e53f16e5c58e99ac7c55438b6522cc
Chippa-a <vusal1372@gmail.com>
2017-04-29 21:43:08 +0300

    yuga: Remove sony-usb init
    Change-Id: I2d08ae1140e73f36cd21cf803050995e0023d1e4

--- product/init.mk
+++ product/init.mk
@@ -1,4 +1,3 @@
 # Init configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.device.rc:root/init.device.rc \
-    $(LOCAL_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc
+    $(LOCAL_PATH)/rootdir/init.device.rc:root/init.device.rc
