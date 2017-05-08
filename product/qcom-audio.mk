b2da7d7ae789fdf6547c2eb0458dbc92e5fb4e44
Chippa-a <vusal1372@gmail.com>
2017-05-08 15:55:36 +0300

    yuga: Switch to XML audio policy configuration
     * Adapted for fusion3 devices policy
     * Cleanup and updates from CAF & stock
    
    Change-Id: I9b868ed6f8cdf72d3a5627452048c7c684290448

---
+++ product/qcom-audio.mk
@@ -0,0 +1,7 @@
+# Audio permissions
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
