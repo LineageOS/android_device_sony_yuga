LineageOS/android_device_sony_huashan

build: Use the global build fingerprint variable
 * Define the global BUILD_FINGERPRINT variable
    to match system/vendor/boot fingerprints

 * Improve properties code style

 * Resolves Android P-DP1 "Android system" warning:
    "There's an internal problem with your device.
     Contact your manufacturer for details."

Change-Id: Ica82390dfe0af554b4096890d08cb5a58a2d4d19

lineage.mk
@@ -32,5 +32,10 @@ PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia SP
 
-# Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
+# Build properties
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=C5303 \
+    PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys

LineageOS/android_device_sony_yuga

lineage.mk
@@ -32,5 +32,10 @@ PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia Z
 
-# Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:5.1.1/10.7.A.0.228/58103698:user/release-keys PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.228 58103698 release-keys"
+# Build properties
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=C6603 \
+    PRIVATE_BUILD_DESC="C6603-user 5.1.1 10.7.A.0.228 58103698 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := Sony/C6603/C6603:5.1.1/10.7.A.0.228/58103698:user/release-keys